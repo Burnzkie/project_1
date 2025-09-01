e85ecb06e2b0217fedbdf30cb0c0326f9154b0bb
Burnzkie <judeburnea20@gmail.com>
2025-09-01 20:25:12 +0800

    adding login/signup Form in html and make function in javascript

---
+++ src/login/login.js
@@ -0,0 +1,7 @@
+const loginBtn = document.querySelector('#login');
+const signupBtn = document.querySelector('#signup');
+const signupForm = document.querySelector('#signupForm');
+const loginForm = document.querySelector('#loginForm');
+const toLogin = document.querySelector('#toLogin');
+const toSignup = document.querySelector('#toSignup');
+
